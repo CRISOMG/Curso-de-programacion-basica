95563d2381085670be9f097d20fba502f4c90058
Cristian <cristiancaraballo112@gmail.com>
2019-12-06 17:31:46 -0400

    ....

--- #6.Proyecto/banco.js
+++ #6.Proyecto/banco.js
@@ -1,17 +1,19 @@
-class Banco {
+class Banco{
 
-  constructor(n, c, s) {
-    this.nombre = n;
-    this.cuenta = c;
-    this.saldo = s;
-  }
-  imprimir() {
+   constructor(n,c,s)
+   {
+     this.nombre = n;
+     this.cuenta = c;
+     this.saldo = s;
+   }
+   imprimir()
+   {
     document.write(`<p>Banco:${this.nombre} <br> cuenta:${this.cuenta} <br> saldo:$${this.saldo}</p>`);
-  }
+   }
 }
 
-class BancoDestino extends Banco {
-  constructor(n, c, s) {
-    super(n, c, s)
+class BancoDestino extends Banco{
+  constructor(n,c,s){
+    super(n,c,s)
   }
 }
