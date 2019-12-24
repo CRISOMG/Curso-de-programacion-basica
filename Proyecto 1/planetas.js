8d1655390d428eb214ba6d39d1dfd081c4baa0a8
Cristian <cristiancaraballo112@gmail.com>
2019-12-24 16:46:45 -0400

    Responsive design hecho

--- Proyecto 1/planetas.js
+++ Proyecto 1/planetas.js
@@ -1,31 +1,31 @@
-//var usuario = Number(prompt("¿Cual es tu peso?"))
+// var usuario = Number(prompt("¿Cual es tu peso?"));
 
-numberController()
-function numberController(){
-  if(!usuario){
-    alert("Coloca un numero porfavor!")
-    usuario = Number(prompt("¿Cual es tu peso?"))
-    numberController()
+numberController();
+function numberController() {
+  if (!usuario) {
+    alert("Coloca un numero porfavor!");
+    usuario = Number(prompt("¿Cual es tu peso?"));
+    numberController();
   }
 }
-function pesoEnMarte(n){
-  var peso = n * 3.7 / 9.8;
-  return peso 
+function pesoEnMarte(n) {
+  var peso = (n * 3.7) / 9.8;
+  return peso;
 }
-const marte = document.querySelector(".marte")
-marte.addEventListener("click", writeM)
+const marte = document.querySelector(".marte");
+marte.addEventListener("click", writeM);
 
-function writeM(){
+function writeM() {
   console.log(usuario);
-  
+
   //marte.innerText = pesoEnMarte(usuario)
 }
 
-function pesoEnJupiter(n){
-  var peso = n * 24.8 / 9.8;
-  return peso 
+function pesoEnJupiter(n) {
+  var peso = (n * 24.8) / 9.8;
+  return peso;
 }
-const jupiter = document.querySelector(".jupiter")
-function writeJ(){
-  jupiter.innerHTML = pesoEnJupiter(usuario)
+const jupiter = document.querySelector(".jupiter");
+function writeJ() {
+  jupiter.innerHTML = pesoEnJupiter(usuario);
 }
